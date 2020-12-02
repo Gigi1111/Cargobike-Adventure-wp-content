660a745268b9906f89e32eeaa231c5b4976dfa63
Gigi1111 <airolg1111@gmail.com>
2020-12-02 18:45:51 +0100

    [styling] slideshow arrows

--- themes/btw_underscore/page-home.php
+++ themes/btw_underscore/page-home.php
@@ -302,11 +302,11 @@ get_header();
 
                 <!-- Left and right controls -->
                 <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left btn"></span>
+                    <span class="glyphicon glyphicon-chevron-left"></span>
                     <span class="sr-only">Previous</span>
                 </a>
                 <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right btn"></span>
+                    <span class="glyphicon glyphicon-chevron-right"></span>
                     <span class="sr-only">Next</span>
                 </a>
             </div>
@@ -403,11 +403,11 @@ get_header();
 
                 <!-- Left and right controls -->
                 <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left btn"></span>
+                    <span class="glyphicon glyphicon-chevron-left"></span>
                     <span class="sr-only">Previous</span>
                 </a>
                 <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right btn"></span>
+                    <span class="glyphicon glyphicon-chevron-right"></span>
                     <span class="sr-only">Next</span>
                 </a>
             </div>
