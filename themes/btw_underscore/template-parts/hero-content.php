ed2c3f63b33b8d0c1e261d2e19171f299a2872aa
Gigi1111 <airolg1111@gmail.com>
2020-12-02 22:03:00 +0100

    [page] add product and gut-zu-wissen pages shell

--- themes/btw_underscore/template-parts/hero-content.php
+++ themes/btw_underscore/template-parts/hero-content.php
@@ -7,25 +7,27 @@ $hero_btn   = get_field('hero_btn');
 
 ?>
 <div class="cover-container d-flex mx-auto flex-column">
-            <div class="cover-inner">
+            <div class="cover-inner" style="width:70%;">
                 <div class="cover-text">
                     <form action="/buchung" method="post">
                         <label for="search">
-                            <h1 style="margin: 20px;"><?php  echo $hero_text; ?></h1></label>
-                            <!-- <h3>Aktiv und nachhaltig Urlauben: Mit E-Cargobikes und (wahlweise) hochwertigem Campingequipment denken wir Roadtrips neu. 
-Paare, Singles, Familien, Hundebesitzer oder Senioren: Zielgruppe ist jeder, der Fahrrad fahren kann. Genießt die Flexibilität des Radurlaubs und nehmt mit, was euch lieb ist. Dafür entwickeln wir derzeit nach ersten erfolgreichen Tests im Spätsommer 2020 einen Lastenradaufbau! 
-Schlauchboot, Grill oder Angelzeug? 
-Der Spontanität und Individualität sind keine Grenzen gesetzt und mit unseren Bikes "purzeln die Kilometer wie Alltagssorgen". Routenvorschläge gibt's auch. 
-Darum können wir den Start der Fahrradsaison kaum erwarten. Den Duft der Natur im Fahrtwind. 
-Wann startet ihr?</h3> -->
-                        <div class="row">
+                            <!-- <h1 style="margin: 20px;"><?php  echo $hero_text; ?></h1> -->
+                            <h1 style="margin: 20px;"><span style="color:var(--brand-secondary);
+                            text-shadow: 0 0 1px var(--brand-primary);
+                            ">Cargobike Adventures</span><br> So geht Roadtrip heute!</h1>
+                            <h3 style="font-weight:300;"> 
+Aktiv und nachhaltig Urlauben: Mit E-Cargobikes und hochwertigem Campingequipment denken wir Roadtrips neu. 
+Paare, Singles, Familien, Hundebesitzer oder Senioren. </h3>
+                        </label>
+                            
+                        <!-- <div class="row">
                             <div class="col-sm-6">
                                 <input type="text" name="daterange" id="daterange" value="27/11/2020 - 05/12/2020" />
                             </div>
-                            <div class="col-sm-6">
+                            <div class="col-sm-6"> -->
                                 <input type="submit" class="btn" id=”searchsubmit” value="<?php  echo $hero_btn; ?>"/>
-                            </div>
-                        </div>
+                            <!-- </div>
+                        </div> -->
                 </div>
             </div>
     </div>
