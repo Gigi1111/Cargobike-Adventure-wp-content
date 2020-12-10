f008135bff6b4bc4152c1617cd4677778adc5cef
Gigi1111 <airolg1111@gmail.com>
2020-12-10 18:06:21 +0100

    [fixing] footer

--- themes/btw_underscore/footer.php
+++ themes/btw_underscore/footer.php
@@ -9,44 +9,13 @@
  * @package BTW
  */
 
- /**
- * Template Name: Footer
- */
-$title1    = get_post_meta(11, 'section_1_title', true);
-$address    = get_post_meta(11, 'company_address', true);
-$contact       = get_post_meta(11, 'company_contact', true);
-$opening_hour        = get_post_meta(11, 'company_opening_hour', true);
-
-$title2    = get_post_meta(11, 'section_2_title', true);
-// $address    = get_post_meta(11, 'company_address', true);
-// $contact       = get_post_meta(11, 'company_contact', true);
-// $opening_hour        = get_post_meta(11, 'company_opening_hour', true);
-
-$title3    = get_post_meta(11, 'section_3_title', true);
-// $address    = get_post_meta(11, 'company_address', true);
-// $contact       = get_post_meta(11, 'company_contact', true);
-// $opening_hour        = get_post_meta(11, 'company_opening_hour', true);
-
-$title4    = get_post_meta(11, 'section_4_title', true);
-$paypal    = get_post_meta(11, 'paypal', true);
-$visa       = get_post_meta(11, 'visa', true);
-$mastercard        = get_post_meta(11, 'mastercard', true);
-
-$title5    = get_post_meta(11, 'section_5_title', true);
-$twitter    = get_post_meta(11, 'twitter', true);
-$facebook       = get_post_meta(11, 'facebook', true);
-$instagram        = get_post_meta(11, 'instagram', true);
-
-$form_text    = get_post_meta(11, 'form_lead_text', true);
-$form_btn    = get_post_meta(11, 'form_btn_text', true);
-
-$booking_text    = get_post_meta(11, 'booking_lead_text', true);
-$booking_btn    = get_post_meta(11, 'booking_btn_text', true);
 ?>
 
-<?php wp_footer(); ?>
+    <!-- FOOTER
+	================================================== -->
+<footer id="site-footer" role="contentinfo" class="header-footer-group">
 
- <!-- image call to action
+    <!-- image call to action
 	================================================== -->
     <?php 
              set_query_var( 'content_text', "Planen Sie jetzt eine unvergessliche Radtour!" );
@@ -58,14 +27,6 @@ $booking_btn    = get_post_meta(11, 'booking_btn_text', true);
   
 
 
-    <!-- FOOTER
-	================================================== -->
-    <section id="optin" >
-            <?php
-            set_query_var( 'content_text', "Planen Sie jetzt eine unvergessliche Radtour!" );
-            set_query_var( 'btn_text', "los geht's" );
-             get_template_part( 'template-parts/section', 'oneline' );?>
-    </section>
 
   
      <!-- PROJECT FEATURES
@@ -73,41 +34,39 @@ $booking_btn    = get_post_meta(11, 'booking_btn_text', true);
     <section id="footer-features" >
         <div class="container">
             <div class="row">
-                <!-- <?php if(!empty($title1)): ?> -->
-                
+
+                 <!-- col -->
                     <div class="col-md-2 col-sm-12">
                         <h5>
-                        <!-- <?php echo $title1; ?> -->
                         Kontakt
                         </h5>
-                            <!-- <?php echo $address; ?> -->
+
                             Cargobike Adventures @ MORE Cargobike GmbH Bouchéstr. 12 | Halle 20 12435 Berlin
 
                             <br><br>
-                            <!-- <?php echo $contact; ?> -->
+
                             0160.2066123 urlaub@cargobike-adventures.de
 
                             <br><br>
-                            <!-- <?php echo $opening_hour; ?> -->
+
 
                             (Nahezu) jederzeit mit vorheriger Terminvereinbarung.
                     </div>
-                <!-- <?php endif; ?> -->
-                <!-- col -->
+              
   
-  
-                <div class="col-md-2 col-sm-12">
-                        <h5>
-                        Gesetz
-                        </h5>
-                         <ul class="nav navbar-nav">
-                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-155"  style="width: 100%;text-align: center;"> 
-                                <a href="<?php echo get_site_url(); ?>/impressum" style="margin:0">Impressum</a>
-                            </li>
-                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-155"  style="width: 100%;text-align: center;">
-                                <a href="<?php echo get_site_url(); ?>/datenschutz" style="margin:0">Datenschutz</a>
-                            </li>
-                         </ul>
+   <!-- col -->
+                    <div class="col-md-2 col-sm-12">
+                            <h5>
+                            Gesetz
+                            </h5>
+                            <ul class="nav navbar-nav">
+                                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-155"  style="width: 100%;text-align: center;"> 
+                                    <a href="<?php echo get_site_url(); ?>/impressum" style="margin:0">Impressum</a>
+                                </li>
+                                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-155"  style="width: 100%;text-align: center;">
+                                    <a href="<?php echo get_site_url(); ?>/datenschutz" style="margin:0">Datenschutz</a>
+                                </li>
+                            </ul>
                     </div>
                 <!-- col -->
                 <div class="col-md-2 col-sm-12">
@@ -123,8 +82,9 @@ $booking_btn    = get_post_meta(11, 'booking_btn_text', true);
                             </li>
                          </ul>
                     </div>
+
                 <!-- col -->
-                <?php if(!empty($title4)): ?>
+
                     <div class="col-md-2 col-sm-12">
                         <h5>Zahlungsmethode</h5>
                         <ul style="list-style-type:none; margin: 0; width:100%; padding: 0;">
@@ -133,26 +93,26 @@ $booking_btn    = get_post_meta(11, 'booking_btn_text', true);
                            </li>
                           
                            <li class="col-sm-12">
-                                Überweisung
+                                <img src="<?php bloginfo('stylesheet_directory');?>/assets/img/payment/transfer2.png" alt="ueberweisung" style="    height: 70px;
+    display: block;
+    float: left;
+    margin-left: 10px;">
                            </li>
                         </ul>
                     </div>
-                <?php endif; ?>
+
                 <!-- col -->
-                <?php if(!empty($title5)): ?>
+
                     <div class="col-md-2 col-sm-12">
-                        <h5><?php echo $title5; ?></h5>
-                        <!-- <?php if(!empty($twitter)): ?>
-                            <a href="<?php echo $twitter; ?>" class="badge social twitter" target="_blank"><i class="fa fa-twitter"></i></a>
-                        <?php endif; ?> -->
-                        <!-- <?php if(!empty($facebook)): ?> -->
+                        <h5>Social Network</h5>
+                    
+                        
                             <a href="https://www.facebook.com/sogehtroadtripheute/photos/104079531466752" class="badge social facebook" target="_blank"><i class="fa fa-facebook"></i></a>
-                        <!-- <?php endif; ?> -->
-                        <!-- <?php if(!empty($instagram)): ?> -->
+                     
                             <a href="https://www.instagram.com/p/CHprhBULxeJ/"  class="badge social facebook" target="_blank"><i class="fa fa-instagram"></i></a>
-                        <!-- <?php endif; ?> -->
+                      
                     </div>
-                <?php endif; ?>
+
                 <!-- col -->
 
             </div>
@@ -161,7 +121,7 @@ $booking_btn    = get_post_meta(11, 'booking_btn_text', true);
         <!-- container -->
     </section>
     <!-- footer-features -->
-
+</footer>
 
  
 
@@ -182,6 +142,8 @@ $booking_btn    = get_post_meta(11, 'booking_btn_text', true);
         } catch (e) {}
     </script>
 
+
+<?php wp_footer(); ?>
 </body>
 
 </html>
--- themes/btw_underscore/page.php
+++ themes/btw_underscore/page.php
@@ -15,25 +15,7 @@
 get_header();
 ?>
 	
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-			<div class="page-wrapper">
-				<div class="page-content" style="text-align:">
-					<div class="">
-					<h1 class="page-title">Page Not Found</h1>
-					<form action="<?php echo get_site_url();?>" method="post">
-                        <label for="search"> 
-                        </label>
-                            <input type="submit" class="btn custom-btn" id=”searchsubmit” value="Back to HOME"/>
-                        </form>
-					</div>
-					
-
-				</div><!-- .page-content -->
-			</div><!-- .page-wrapper -->
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
+	
 
 <?php
 get_footer();
